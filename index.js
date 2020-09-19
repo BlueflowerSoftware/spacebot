15bafdcac83feb50ec0b80d796404f2ee47862af
GuilhemDeMarco <55885506+GuilhemDeMarco@users.noreply.github.com>
2020-09-20 01:40:32 +0200

    experiments on movement. will be removed and will be modified

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const client = new Discord.Client();
 
 const config = require("./config.json");
 
+var currentlyMovingPlayers = [];
+
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -21,6 +23,63 @@ client.on('message', async message => {
 
   console.log(command);
   console.log(args);
+
+	if (command == "goto") {
+		let playerID = message.author.id;
+		let playerDestinationPosition = [args[0],args[1]];
+
+		currentlyMovingPlayers.push([playerID,playerDestinationPosition])
+
+		//currentlyMoving.push(message.author.id);
+		//console.log(currentlyMoving);
+	}
 })
 
 client.login(config.token);
+
+// !! MAIN LOOP !!
+setInterval(() => {
+	movePlayers()
+}, 1000)
+
+let playerCurrentPosition = []
+function movePlayers() {
+	if (currentlyMovingPlayers.length != 0) {
+		//console.log(currentlyMoving);
+		for (var movingPlayer of currentlyMovingPlayers) {
+			//console.log(movingPlayer);
+			if (playerCurrentPosition.length == 0) {
+				// TEMP: temporary variable
+				playerCurrentPosition = [0,0];
+			}
+			let playerDestinationPosition = movingPlayer[1];
+			console.log(playerCurrentPosition, playerDestinationPosition);
+			let journeyLength = Math.round(Math.hypot(playerCurrentPosition[0]-playerDestinationPosition[0],
+				 playerCurrentPosition[1]-playerDestinationPosition[1])) //Math.hypot(x2-x1, y2-y1)
+
+			console.log('length: ', journeyLength);
+
+			const velocity = 1;
+
+			let deltaX = playerDestinationPosition[0] - playerCurrentPosition[0]
+			let deltaY = playerDestinationPosition[1] - playerCurrentPosition[1]
+			let angle = Math.atan(deltaY, deltaX)
+
+			let xVelocity = velocity * Math.cos(angle);
+			let yVelocity = velocity * Math.sin(angle);
+			//console.log(xVelocity, yVelocity);
+
+			playerCurrentPosition[0] += xVelocity;
+			playerCurrentPosition[1] += yVelocity;
+
+			if (journeyLength <= 0) {
+				const index = currentlyMovingPlayers.indexOf(movingPlayer);
+					if (index > -1) {
+  					currentlyMovingPlayers.splice(index, 1);
+					}
+			}
+
+		}
+		console.log('moving');
+	}
+}
