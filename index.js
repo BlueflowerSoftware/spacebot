432036b7f7fbe929a12d791fc7e19414240c0ff2
GuilhemDeMarco <55885506+GuilhemDeMarco@users.noreply.github.com>
2020-09-19 19:59:58 +0200

    basic discord.js stuff

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const config = require("./config.json");
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', async message => {
+  if(message.author.bot) return;
+  // This game can only be played in the bot's dms
+  if(!(message.channel.type === "dm")) return;
+
+  console.log(message.content);
+
+  let contentArray = message.content.split(/ +/g);
+  const command = contentArray[0];
+  contentArray.shift()
+  const args = contentArray
+
+  console.log(command);
+  console.log(args);
+})
+
+client.login(config.token);
